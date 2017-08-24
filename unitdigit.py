b72b4eec9e2632351698bc424735d3647d75f0c4
shivamsam050 <31090066+shivamsam050@users.noreply.github.com>
2017-08-23 22:18:42 -0700

    Add files via upload

---
+++ unitdigit.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 24 10:18:47 2017
+
+@author: sharda
+"""
+
+n=int(input("enter the number to get its unit digit="))
+c=n%10
+print('unit digit of number ',n,' is',c)
